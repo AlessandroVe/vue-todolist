8427b5b364dd84dc63d2c65a4b0ba9583c2b1cdb
AlessandroVe <aveneziano536@gmail.com>
2021-07-01 04:58:13 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const app =  new Vue({
     el:"#app",
     data:{
+        toDoLIst:["comprare le mele", "andare dal fabbro", "comprare le cicche "]
 
     },
     created(){
@@ -9,7 +10,14 @@ const app =  new Vue({
 
     },
     methods:{
-        
+        indexPlus : function(index){
+            return index+1
+        },
+        cross : function(index){
+            if(index % 2 ===0){
+                return "far fa-times-circle"
+            }
+        }
         
     }
 }
