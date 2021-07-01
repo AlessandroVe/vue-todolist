9d44fc87aecc3ef54d899b1baf3a6cfb32d196e3
AlessandroVe <aveneziano536@gmail.com>
2021-07-01 05:30:36 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const app =  new Vue({
     el:"#app",
     data:{
-        toDoLIst:["comprare le mele", "andare dal fabbro", "comprare le cicche "]
-
+        toDoLIst:["comprare le mele", "andare dal fabbro", "comprare le cicche ",],
+        newTask:"",
     },
     created(){
 
@@ -15,8 +15,13 @@ const app =  new Vue({
         },
         cross : function(index){
             if(index % 2 ===0){
+                return "far fa-check-circle "
+            }else{
                 return "far fa-times-circle"
             }
+        },
+        addTask: function (){
+            this.toDoLIst.push(this.newTask)
         }
         
     }
